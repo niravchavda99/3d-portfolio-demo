def7fcb25f126b587c06a1646e8eb4dbfdf08f0e
Nirav Chavda <nmchavda99@gmail.com>
2023-11-18 20:18:58 +0530

    Music

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import {Canvas} from "@react-three/fiber";
-import {Suspense, useState} from "react";
+import {Suspense, useEffect, useRef, useState} from "react";
 import {Loader} from "../components/Loader.tsx";
 import {Island} from "../models/Island.tsx";
 import {Sky} from "../models/Sky.tsx";
@@ -7,9 +7,36 @@ import {Bird} from "../models/Bird.tsx";
 import {Plane} from "../models/Plane.tsx";
 import {HomeInfo} from "../components/HomeInfo.tsx";
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+import sakura from '../assets/sakura.mp3';
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+import soundOn from '../assets/icons/soundon.png';
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+import soundOff from '../assets/icons/soundoff.png';
+
 export function Home() {
+    const audioRef = useRef(new Audio(sakura));
+    audioRef.current.volume = 0.4;
+    audioRef.current.loop = true;
+
+    const [isPlayingMusic, setIsPlayingMusic] = useState<boolean>(false);
     const [isRotating, setIsRotating] = useState<boolean>(false);
     const [currentStage, setCurrentStage] = useState<number>(1);
+
+    useEffect(() => {
+        if (isPlayingMusic) {
+            audioRef.current.play();
+        }
+
+        return () => {
+            audioRef.current.pause();
+        };
+    }, [isPlayingMusic]);
 
     const adjustIslandForScreenSize = () => {
         const isSmallScreen = window.innerWidth < 768;
@@ -33,7 +60,7 @@ export function Home() {
     return (
         <section className='w-full h-screen relative'>
             <div className='absolute top-28 left-0 right-0 z-10 flex items-center justify-center'>
-                {currentStage && <HomeInfo currentStage={currentStage} />}
+                {currentStage && <HomeInfo currentStage={currentStage}/>}
             </div>
             <Canvas className={`w-full h-screen bg-transparent ${isRotating ? 'cursor-grabbing' : 'cursor-grab'}`}
                     camera={{near: 0.1, far: 1000}}
@@ -61,6 +88,14 @@ export function Home() {
                         isRotating={isRotating}/>
                 </Suspense>
             </Canvas>
+            <div className='absolute bottom-2 left-2'>
+                <img
+                    src={isPlayingMusic ? soundOn : soundOff}
+                    alt="Sound Icon"
+                    className='w-10 h-10 cursor-pointer object-contain'
+                    onClick={() => setIsPlayingMusic(!isPlayingMusic)}
+                />
+            </div>
         </section>
     );
 }
